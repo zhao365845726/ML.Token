4a48c8a8f4712bf676c82f639a934e77ba098e64
zhao365845726@163.com <zhao365845726@163.com>
2023-02-24 09:08:41 +0800

    优化Token帮助类的内容

--- ML.Token/YonYou/TokenHelper.cs
+++ ML.Token/YonYou/TokenHelper.cs
@@ -13,25 +13,40 @@ namespace ML.Token.YonYou
         /// <summary>
         /// 【Use】企业自建
         /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="ysEnv"></param>
+        /// <param name="isUseRedis">是否使用Redis存储</param>
         /// <returns></returns>
-        public static string GetSelfAccessToken(string filePath,string ysEnv)
+        public static string GetSelfAccessToken(string filePath,string ysEnv,bool isUseRedis)
         {
             System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1)); // 当地时区
             long timeStamp = (long)(DateTime.Now - startTime).TotalSeconds; // 相差秒数
             AccessTokenRequest request = JsonHelper.GetConfigInfo<AccessTokenRequest>(filePath, ysEnv);
             request.timestamp = timeStamp.ToString();
-            //TODO:获取Redis的AccessToken，如果存在，则直接返回，如果不存在，再重新创建
-            string strAccessToken = RedisHelper.Get<string>($"accessToken:{request.appKey}");
-            if (strAccessToken != null)
+
+            //TODO:使用Redis读取
+            if (isUseRedis)
             {
-                return strAccessToken;
+                //TODO:获取Redis的AccessToken，如果存在，则直接返回，如果不存在，再重新创建
+                string strAccessToken = RedisHelper.Get<string>($"accessToken:{request.appKey}");
+                if (strAccessToken != null)
+                {
+                    return strAccessToken;
+                }
             }
+            
             string requestUrl = $"https://api.diwork.com/open-auth/selfAppAuth/getAccessToken?appKey={request.appKey}&timestamp={request.timestamp}&signature={CounterSign(request)}";
             var result = HttpHelper.Get(requestUrl);
             var model = JsonConvert.DeserializeObject<IDictionary<string, object>>(result.ToString());
             var response = JsonConvert.DeserializeObject<IDictionary<string, object>>(model["data"].ToString());
-            //加入Redis
-            RedisHelper.Set($"accessToken:{request.appKey}", response["access_token"].ToString(), Convert.ToInt32(response["expire"].ToString()));
+
+            //TODO:使用Redis存储
+            if (isUseRedis)
+            {
+                //加入Redis
+                RedisHelper.Set($"accessToken:{request.appKey}", response["access_token"].ToString(), Convert.ToInt32(response["expire"].ToString()));
+            }
+            
             return response["access_token"].ToString();
         }
 
